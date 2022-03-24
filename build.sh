c704e7dce06eab9996be6ca7d97110351333cd50
Magenta Puppy <theverymagentapuppy@gmail.com>
2022-03-24 14:03:08 +1100

    modified:   build.sh 	modified:   config.yml 	new file:   content/how-to-set-up-remark.md 	new file:   static/profile-banner.png 	modified:   themes/papermod (modified content)

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 sudo systemctl stop caddy
-caddy stop
-sleep 0.3
+sleep 0.05
 rm /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/Magenta-blog-Public/* -r
-sleep 0.3
+sleep 0.05
 cd /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/magenta-blog
-sleep 0.3
+sleep 0.05
 hugo
-sleep 0.3
+sleep 0.05
 cp Caddyfile /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/Magenta-blog-Public
-sleep 0.3
+sleep 0.05
 #caddy start --config /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/Magenta-blog-Public/Caddyfile
 sudo systemctl start caddy
