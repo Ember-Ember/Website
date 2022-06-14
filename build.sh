4067a8bf869f179445b7fc937872d2109fe3ff13
Emberdev <github@emberdev.me>
2022-06-14 20:53:53 +1000

    updated shit

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 sudo systemctl stop caddy
 sleep 0.05
-rm /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/Magenta-blog-Public/* -r
+rm /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/ember-blog-Public/* -r
 sleep 0.05
-cd /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/magenta-blog
+cd /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/ember-blog
 sleep 0.05
 hugo
 sleep 0.05
-cp Caddyfile /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/Magenta-blog-Public
+cp Caddyfile /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/ember-blog-Public
 sleep 0.05
 #caddy start --config /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/Magenta-blog-Public/Caddyfile
 sudo systemctl start caddy
