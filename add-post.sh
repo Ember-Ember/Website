9b1ae024a0b7a74c2dbbd93fa1baac5d1bd322e7
Emberdev <github@emberdev.me>
2022-06-14 20:53:53 +1000

    updated shit

--- add-post.sh
+++ add-post.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/magenta-blog
+cd /mnt/ddf46e84-10c6-40fb-85b2-99f14e503946/selfhost/ember-blog
 
 echo "Enter post name and location relative to the content directory (posts/post-name for a regular post) (.md is added automatically)"
 
